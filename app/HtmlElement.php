8df372ad26e300e693965aa4b1f8b3e3e8610885
Israel Nieto <isra.utch@gmail.com>
2019-10-21 09:15:00 -0600

    Programación estructurada

--- app/HtmlElement.php
+++ app/HtmlElement.php
@@ -6,8 +6,60 @@ namespace App;
 
 class HtmlElement
 {
+    /**
+     * @var string
+     */
+    private $name;
+    /**
+     * @var null
+     */
+    private $content;
+    /**
+     * @var array
+     */
+    private $attributes;
+
+    public function __construct($name , $content = null, array $attributes = [])
+    {
+
+        $this->name = $name;
+        $this->content = $content;
+        $this->attributes = $attributes;
+    }
+
     public function render()
     {
-        return 'HTML ELEMENT HERE';
+        //Si el elemento tiene atributes:
+        if (! empty($this->attributes)) {
+            $htmlAttributes = '';
+
+            foreach ($this->attributes as $attribute => $value) {
+                if (is_numeric($attribute)) {
+                    $htmlAttributes .= ' ' .$value;
+                }else {
+                    $htmlAttributes .= ' '.$attribute .'="' . htmlentities($value, ENT_QUOTES, 'UTF-8') . '"';
+                }
+            }
+            //Abrir la etiqueta con atributos
+            $result = '<' .$this->name . $htmlAttributes .'>';
+        }else {
+
+            //Abrir la etiqueta sin atributos
+            $result = '<' .$this->name . '>';
+        }
+
+
+        //Si el elemento es void
+        if (in_array($this->name, ['br','hr', 'img' , 'input' , 'meta'])) {
+            return $result;
+        }
+
+        //Imprimir el contenido
+        $result .= htmlentities($this->content, ENT_QUOTES, 'UTF-8');
+        //Cerrar etiqueta
+        $result .= '</' . $this->name . '>';
+
+
+        return $result;
     }
 }
--- public/index.php
+++ public/index.php
@@ -2,6 +2,36 @@
 
 require '../vendor/autoload.php';
 
-$element =  new \App\HtmlElement();
+$element =  new \App\HtmlElement('p', 'Hola mundo', []);
 
-echo $element->render();
+echo htmlentities($element->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element1 =  new \App\HtmlElement('p', 'Hola mundo ', ['id' => 'my_paragraph']);
+
+echo htmlentities($element1->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element2 =  new \App\HtmlElement('p', 'Hola mundo ', ['id' => 'my_paragraph', 'class' => 'paragraph']);
+
+echo htmlentities($element2->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element2 =  new \App\HtmlElement('img', null, ['src' => '/img/image.png']);
+
+echo htmlentities($element2->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element2 =  new \App\HtmlElement('img', null, ['src' => '/img/image.png', 'title' => 'Curso de "Refactorización" en Styde']);
+
+echo htmlentities($element2->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element2 =  new \App\HtmlElement('input', null, ['required']);
+
+echo htmlentities($element2->render(), ENT_QUOTES, 'UTF-8');
