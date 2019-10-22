3e405c034e20ff49854517785d23a4e112daa8d8
Israel Nieto <isra.utch@gmail.com>
2019-10-22 09:22:47 -0600

    Calistenia de objetos

--- app/HtmlElement.php
+++ app/HtmlElement.php
@@ -29,37 +29,73 @@ class HtmlElement
 
     public function render()
     {
-        //Si el elemento tiene atributes:
-        if (! empty($this->attributes)) {
-            $htmlAttributes = '';
+        $result = $this->openTag();
 
-            foreach ($this->attributes as $attribute => $value) {
-                if (is_numeric($attribute)) {
-                    $htmlAttributes .= ' ' .$value;
-                }else {
-                    $htmlAttributes .= ' '.$attribute .'="' . htmlentities($value, ENT_QUOTES, 'UTF-8') . '"';
-                }
-            }
-            //Abrir la etiqueta con atributos
-            $result = '<' .$this->name . $htmlAttributes .'>';
-        }else {
-
-            //Abrir la etiqueta sin atributos
-            $result = '<' .$this->name . '>';
-        }
-
-
-        //Si el elemento es void
-        if (in_array($this->name, ['br','hr', 'img' , 'input' , 'meta'])) {
+        if ($this->isVoid()) {
             return $result;
         }
 
-        //Imprimir el contenido
-        $result .= htmlentities($this->content, ENT_QUOTES, 'UTF-8');
-        //Cerrar etiqueta
-        $result .= '</' . $this->name . '>';
+        $result .= $this->content();
 
+        $result .= $this->closeTag();
 
         return $result;
+    }
+
+    protected function openTag()
+    {
+        if (! empty($this->attributes)) {
+            $result = '<' .$this->name . $this->attributes() .'>';
+        }else {
+            $result = '<' .$this->name . '>';
+        }
+
+        return $result;
+    }
+
+    public function attributes()
+    {
+        $htmlAttributes = '' ;
+
+        foreach ($this->attributes as $attribute => $value) {
+            $htmlAttributes .= $this->renderAttributes($attribute, $value);
+        }
+
+        return $htmlAttributes;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isVoid()
+    {
+        return in_array($this->name, ['br', 'hr', 'img', 'input', 'meta']);
+    }
+
+    protected function renderAttributes( $attribute, $value)
+    {
+        if (is_numeric($attribute)) {
+            $htmlAttributes = ' ' .$value;
+        }else {
+            $htmlAttributes = ' '.$attribute .'="' . htmlentities($value, ENT_QUOTES, 'UTF-8') . '"';
+        }
+
+        return $htmlAttributes;
+    }
+
+    /**
+     * @return string
+     */
+    public function content()
+    {
+        return htmlentities($this->content, ENT_QUOTES, 'UTF-8');
+    }
+
+    /**
+     * @return string
+     */
+    public function closeTag()
+    {
+        return '</' . $this->name . '>';
     }
 }
--- tests/HtmlElementTest.php
+++ tests/HtmlElementTest.php
@@ -4,11 +4,84 @@
 namespace Tests;
 
 
+use App\HtmlElement;
+
 class HtmlElementTest extends TestCase
 {
     /** @test **/
-    function it_assert_true()
+    function it_checks_if_a_element_is_void_or_not()
     {
-        $this->assertTrue(true);
+        $this->assertTrue((new \App\HtmlElement('img'))->isVoid());
+
+        $this->assertFalse((new \App\HtmlElement('p'))->isVoid());
+    }
+    
+    /** @test **/
+    function it_generate_attributes()
+    {
+        $element = new HtmlElement('span', null,  ['class' => 'blue', 'id' => 'my_span']);
+
+        $this->assertSame(' class="blue" id="my_span"', $element->attributes());
+    }
+
+    /** @test **/
+    function it_generates_a_paragraph_with_content()
+    {
+        $element =  new \App\HtmlElement('p', 'Hola mundo', []);
+
+        $this->assertSame('<p>Hola mundo</p>' ,$element->render());
+    }
+
+    /** @test **/
+    function it_generates_a_paragraph_with_content_and_attribute()
+    {
+        $element =  new \App\HtmlElement('p', 'Hola mundo ', ['id' => 'my_paragraph']);
+
+        $this->assertSame('<p id="my_paragraph">Hola mundo </p>' ,$element->render());
+    }
+
+    /** @test **/
+    function it_generates_a_paragraph_with_content_and_more_attributes()
+    {
+        $element =  new \App\HtmlElement('p', 'Hola mundo ', ['id' => 'my_paragraph', 'class' => 'paragraph']);
+
+        $this->assertSame('<p id="my_paragraph" class="paragraph">Hola mundo </p>' ,$element->render());
+    }
+
+    /** @test **/
+    function it_generates_a_image()
+    {
+        $element =  new \App\HtmlElement('img', null, ['src' => '/img/image.png']);
+
+        $this->assertSame('<img src="/img/image.png">' ,$element->render());
+    }
+
+    /** @test **/
+    function it_generates_a_image_with_attributes_and_escaped()
+    {
+        $element =  new \App\HtmlElement(
+            'img', null, ['src' => '/img/image.png', 'title' => 'Curso de "Refactorización" en Styde']
+        );
+
+        $this->assertSame(
+            '<img src="/img/image.png" title="Curso de &quot;Refactorizaci&oacute;n&quot; en Styde">' ,
+            $element->render()
+        );
+    }
+
+    /** @test **/
+    function it_generates_a_input_and_required()
+    {
+        $element =  new \App\HtmlElement('input', null, ['required']);
+
+        $this->assertSame('<input required>' , $element->render());
+    }
+
+    /** @test **/
+    function it_method_close_tag_close_correctly()
+    {
+        $element =  new \App\HtmlElement('p','Hola mundo');
+
+        $this->assertSame('<p>Hola mundo</p>' , '<p>Hola mundo'. $element->closeTag());
     }
 }
